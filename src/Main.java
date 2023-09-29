977f21ff51541343b1dd96d4fe842dc09e1656bb
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2023-09-29 13:48:04 +0300

    Change if else blocks with metods

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to MineSweeper game");
-        System.out.println("Please enter the size you want to play");
-        System.out.print("Row Number : ");
-        int rowNumber=input.nextInt(); // Request the number of rows from the user.
-        System.out.print("Column Number : ");
-        int columnNumber=input.nextInt();  // Request the number of columns from the user.
-        if (rowNumber>1&&columnNumber>1){
-            MineSweeper mineSweeper = new MineSweeper(rowNumber,columnNumber); // Create a MineSweeper game instance with the user-defined grid size.
-            mineSweeper.run();
-        }
+        MineSweeper.start();
     }
 }
--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -8,6 +8,7 @@ public class MineSweeper {
     int winCondition; // Represents the number of moves required to win the game.
     boolean isOver ; // Flag to indicate if the game is over.
     boolean isWin; // Flag to indicate if the game is won.
+    int counter;
 
     MineSweeper(int row, int column){
         this.row=row;
@@ -32,7 +33,21 @@ public class MineSweeper {
             }
         }
     }
-
+    // Değerlendirme Formu 7
+    public static void start(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to MineSweeper game");
+        System.out.println("Please enter the size you want to play");
+        System.out.print("Row Number : ");
+        int rowNumber=input.nextInt(); // Request the number of rows from the user.
+        System.out.print("Column Number : ");
+        int columnNumber=input.nextInt();  // Request the number of columns from the user.
+        if (rowNumber>1&&columnNumber>1){
+            MineSweeper mineSweeper = new MineSweeper(rowNumber,columnNumber); // Create a MineSweeper game instance with the user-defined grid size.
+            mineSweeper.run();
+        }
+    }
+    //Değerlendirme Formu 8
     public void placeMines(){  // Randomly places mines on the game board.
         int minesPlaced=0;
         while(minesPlaced<mines){
@@ -62,7 +77,7 @@ public class MineSweeper {
             }
         }
     }
-
+    //Değerlendirme Formu 9
     public void run(){   // Runs the game loop, allowing the player to make selections.
         Scanner input=new Scanner(System.in);
         do{
@@ -83,104 +98,88 @@ public class MineSweeper {
             print(gameBoard);
         }
     }
+    public void checkTop(int x,int y){
+        if(x!=0){
+            if(gameBoard[x-1][y].equals("*")){
+                counter++;
+            }
+        }
+    }
+    public void checkBottom(int x,int y){
+        if(x!=row-1){
+            if(gameBoard[x+1][y].equals("*")){
+                counter++;
+            }
+        }
+    }
+    public void checkRight(int x,int y){
+        if(y!=column-1){
+            if(gameBoard[x][y+1].equals("*")){
+                counter++;
+            }
+        }
+    }
+    public void checkLeft(int x,int y){
+        if(y!=0){
+            if(gameBoard[x][y-1].equals("*")){
+                counter++;
+            }
+        }
+    }
+    public void checkLeftTop(int x,int y){
+        if(y!=0&&x!=0){
+            if(gameBoard[x-1][y-1].equals("*")){
+                counter++;
+            }
+        }
+    }
+    public void checkLeftBottom(int x,int y){
+        if(x!=row-1&&y!=0){
+            if(gameBoard[x+1][y-1].equals("*")){
+                counter++;
+            }
+        }
+    }
+    public void checkRightTop(int x,int y){
+        if(x!=0&&y!=column-1){
+            if(gameBoard[x-1][y+1].equals("*")){
+                counter++;
+            }
+        }
 
-    public void select(int x,int y){   // Handles the player's selection and updates the game board.
+    }
+    public void checkRightBottom(int x,int y){
+        if(y!=column-1&&x!=row-1){
+            if(gameBoard[x+1][y+1].equals("*")){
+                counter++;
+            }
+        }
+
+    }
+
+    public void select(int x,int y){
         int a=x-1;
         int b=y-1;
-        int count=0;
         if(a>=row||b>=column||a<=-1||b<=-1){   // Check if the player's selection is outside the coordinate system.
             System.out.println("You made a selection outside the coordinate system, try again.");
         }else if(!gameBoard[a][b].equals("-")&&!gameBoard[a][b].equals("*")) { // Check if the player has previously selected this coordinate.
             System.out.println("You have entered this coordinate before. Try again.");
         }else if(gameBoard[a][b].equals("*")){ // Check if the player stepped on a mine.
             isOver=true;
-        }else{
-            if(winCondition>0) {
+        }else {
+            if (winCondition > 0) {
+                counter=0;
                 winCondition--;
-
-                    if(a>0&&b>0){
-                        if(a==row-1&&b==column-1){
-                            for(int i=a-1; i<a+1;i++){
-                                for(int j=b-1;j<b+1;j++){
-                                    if(gameBoard[i][j].equals("*")){
-                                        count++;
-                                    }
-                                }
-                            }
-                        } else if(b==column-1&&a!=row-1){
-                            for(int i=a-1; i<a+2;i++){
-                                for(int j=b-1;j<=b;j++){
-                                    if(gameBoard[i][j].equals("*")){
-                                        count++;
-                                    }
-                                }
-                            }
-                        }else if (a==row-1&&b!=column-1) {
-                            for(int i=a-1; i<=a;i++){
-                                for(int j=b-1;j<b+2;j++){
-                                    if(gameBoard[i][j].equals("*")){
-                                        count++;
-                                    }
-                                }
-                            }
-                        }else{
-                            for(int i=a-1; i<a+2;i++){
-                                for(int j=b-1;j<b+2;j++){
-                                    if(gameBoard[i][j].equals("*")){
-                                        count++;
-                                    }
-                                }
-                            }
-                        }
-                    }else if(a==0&&b>0){
-                        if(b==column-1){
-                            for(int i=a; i<a+2;i++){
-                                for(int j=b-1;j<=b;j++){
-                                    if(gameBoard[i][j].equals("*")){
-                                        count++;
-                                    }
-                                }
-                            }
-                        }else{
-                            for(int i=a; i<a+2;i++){
-                                for(int j=b-1;j<b+2;j++){
-                                    if(gameBoard[i][j].equals("*")){
-                                        count++;
-                                    }
-                                }
-                            }
-                        }
-                    }else if(a>0&&b==0){
-                        if(a==row-1){
-                            for(int i=a-1; i<=a;i++){
-                                for(int j=b;j<b+2;j++){
-                                    if(gameBoard[i][j].equals("*")){
-                                        count++;
-                                    }
-                                }
-                            }
-                        }else{
-                            for(int i=a-1; i<a+2;i++){
-                                for(int j=b;j<b+2;j++){
-                                    if(gameBoard[i][j].equals("*")){
-                                        count++;
-                                    }
-                                }
-                            }
-                        }
-
-                    }else{  //b==0&&a==0
-                        for(int i=a; i<a+2;i++){
-                            for(int j=b;j<b+2;j++){
-                                if(gameBoard[i][j].equals("*")){
-                                    count++;
-                                }
-                            }
-                        }
-                    }
-
-                gameBoard[a][b]=""+count;
-                playerBoard[a][b]=""+count;
+                checkLeft(a,b);
+                checkRight(a,b);
+                checkTop(a,b);
+                checkBottom(a,b);
+                checkRightBottom(a,b);
+                checkRightTop(a,b);
+                checkLeftBottom(a,b);
+                checkLeftTop(a,b);
+                gameBoard[a][b]=""+counter;
+                playerBoard[a][b]=""+counter;
             }
             if(winCondition==0){
                 isWin=true;
